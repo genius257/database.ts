1cae3fc739d4fd011de45b42e9fa0b15096c0fdc
Anders Pedersen <hot415@hotmail.com>
2023-08-21 00:47:53 +0000

    Added export of WhereOfType

--- src/Query/Builder.ts
+++ src/Query/Builder.ts
@@ -1,1 +1,1 @@
-export { Builder as default, type Aggregate, type Order, type Union, type Where, type Having } from ".";
+export { Builder as default, type Aggregate, type Order, type Union, type Where, type WhereOfType, type Having } from ".";

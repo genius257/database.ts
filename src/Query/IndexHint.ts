1ac9f13d8306272ca2aae6ff4eae1052d8a38025
Anders Pedersen <hot415@hotmail.com>
2023-08-24 20:51:35 +0000

    Added IndexHint code implementation

--- src/Query/IndexHint.ts
+++ src/Query/IndexHint.ts
@@ -1,3 +1,12 @@
 export default class IndexHint {
-    //
+    /** The type of query hint. */
+    public type: string;//FIXME: a string uinon of known values?
+
+    /** The name of the index. */
+    public index: string;
+
+    public constructor(type: string, index: string) {
+        this.type = type;
+        this.index = index;
+    }
 }
